0c968badaa4c86d13087385a991a9a2867aa105d
MephistoDevelop <inesherald@gmail.com>
2020-01-16 16:08:41 -0600

    Add layouts

--- src/body.js
+++ src/body.js
@@ -1,9 +1,35 @@
+import { mySideBar } from './navbar.js';
+
 function mainContainer() {
-  mainContainer = document.createElement('div');
-  mainContainer.className = 'container';
+  const mainContainer = document.createElement('div');
+  const headerContainer = document.createElement('div');
+  const contentContainer = document.createElement('div');
+  const todosContainer = document.createElement('div');
+  const rowContainer = document.createElement('div');
+
+  rowContainer.id = 'row-container';
+  contentContainer.id = 'content-container';
+  headerContainer.id = 'header-container';
   mainContainer.id = 'main-container';
+  todosContainer.id = 'todo-container';
+
+  todosContainer.textContent = 'Hello im todo List Container';
+  headerContainer.textContent = 'To Do Task Manager';
   document.body.appendChild(mainContainer);
-  console.log('im the body element');
+  mainContainer.appendChild(headerContainer);
+  mainContainer.appendChild(contentContainer);
+  contentContainer.appendChild(rowContainer);
+  mySideBar();
+  rowContainer.appendChild(todosContainer);
+  insideTodoContent();
+}
+function insideTodoContent() {
+  const c = document.getElementById('todo-container');
+  const todoContent = document.createElement('div');
+
+  todoContent.id = 'todo-list-content';
+  todoContent.innerText = 'im the content';
+  c.appendChild(todoContent);
 }
 
 export { mainContainer };
--- src/navbar.js
+++ src/navbar.js
@@ -1,8 +1,20 @@
 function mySideBar() {
+  const mainContainer = document.getElementById('row-container');
   const navContainer = document.createElement('div');
-
-  navContainer.textContent('Hello im the Div Generated ');
-  navContainer.className = 'container';
+  navContainer.id = 'nav-container';
+  mainContainer.appendChild(navContainer);
+  renderList();
 }
 
+function renderList() {
+  const navContainer = document.getElementById('nav-container');
+  const buttonAddContainer = document.createElement('div');
+  const listContainer = document.createElement('div');
+  buttonAddContainer.id = 'btn-add-list';
+  listContainer.id = 'list-container';
+  buttonAddContainer.innerText = '+ Add List';
+  listContainer.innerText = '- im the list of projecs';
+  navContainer.appendChild(buttonAddContainer);
+  navContainer.appendChild(listContainer);
+}
 export { mySideBar };
