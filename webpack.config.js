76a41f9ebbaffcc72774867f108edba7fa5e5d87
MephistoDevelop <inesherald@gmail.com>
2020-01-16 05:31:55 -0600

    Add bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -12,9 +12,25 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: function() {
+                return [require('precss'), require('autoprefixer')];
+              },
+            },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
         test: /\.(png|svg|jpg|gif)$/,
         use: ['file-loader'],
       },
