4cd0188e21513a14aa850c95ec06fa4a0f24bf97
MephistoDevelop <inesherald@gmail.com>
2020-01-17 00:52:46 -0600

    Dynamical content

--- src/navbar.js
+++ src/navbar.js
@@ -13,8 +13,66 @@ function renderList() {
   buttonAddContainer.id = 'btn-add-list';
   listContainer.id = 'list-container';
   buttonAddContainer.innerText = '+ Add List';
-  listContainer.innerText = '- im the list of projecs';
   navContainer.appendChild(buttonAddContainer);
   navContainer.appendChild(listContainer);
+
+  buttonAddContainer.onclick = showMenuAdd;
+}
+function showMenuAdd() {
+  const addContainer = document.getElementById('btn-add-list');
+  const nameListBox = document.createElement('input');
+  const plusBtn = document.createElement('button');
+  const deleteBtn = document.createElement('button');
+
+  plusBtn.id = 'btn-list-plus';
+  deleteBtn.id = 'btn-list-delete';
+
+  plusBtn.innerText = '+';
+  deleteBtn.innerText = 'X';
+  nameListBox.placeholder = 'Name of List';
+  addContainer.innerText = '';
+
+  addContainer.appendChild(nameListBox);
+  addContainer.appendChild(plusBtn);
+  addContainer.appendChild(deleteBtn);
+  addContainer.onclick = '';
+  plusBtn.onclick = function() {
+    showAddBtn();
+    addItemToList();
+  };
+}
+
+function showAddBtn() {
+  const addContainer = document.getElementById('btn-add-list');
+  console.log(addContainer.childElementCount);
+  addContainer.children[0].remove();
+  addContainer.children[1].remove();
+  addContainer.children[0].remove();
+  addContainer.innerText = '+ Add List';
+}
+function addItemToList() {
+  const listContainer = document.getElementById('list-container');
+  const listItem = document.createElement('div');
+  const itemName = document.createElement('div');
+  const showItem = document.createElement('div');
+  const deleteItem = document.createElement('div');
+  const hideContainer = document.createElement('div');
+
+  itemName.innerText = 'My List';
+  deleteItem.innerText = 'X';
+  showItem.innerText = 'Show';
+  listItem.className = 'd-flex';
+
+  hideContainer.className = 'hide-container';
+  itemName.className = 'list-title  ';
+  showItem.className = 'show-btn';
+  deleteItem.className = 'delete-btn';
+
+  listItem.appendChild(itemName);
+  hideContainer.appendChild(showItem);
+  hideContainer.appendChild(deleteItem);
+
+  listItem.appendChild(hideContainer);
+  listContainer.appendChild(listItem);
 }
 export { mySideBar };
