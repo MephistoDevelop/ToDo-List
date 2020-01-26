0c28d99ad6bcab146c6450cda5518eda49bfe179
MephistoDevelop <inesherald@gmail.com>
2020-01-26 01:02:31 -0600

    Fix issues caused by eslint errors

--- src/navbar.js
+++ src/navbar.js
@@ -1,48 +1,38 @@
+/* eslint-disable no-use-before-define */
 import showMessage from './messages';
 // eslint-disable-next-line import/no-cycle
 import { allLists, allTasks, cleanTasks } from './todos';
 
-function addItemToList(name) {
-  const listContainer = document.getElementById('list-container');
-  const listItem = document.createElement('div');
-  const itemName = document.createElement('div');
-  const showItem = document.createElement('div');
-  const deleteItem = document.createElement('div');
-  const hideContainer = document.createElement('div');
+function renderList() {
+  const navContainer = document.getElementById('nav-container');
+  const titleNav = document.createElement('h3');
+  const buttonAddContainer = document.createElement('div');
+  const listContainer = document.createElement('div');
+  const textAdd = document.createElement('div');
 
-  itemName.innerText = `${name}`;
-  deleteItem.innerText = 'X';
-  showItem.innerText = 'Show';
-  listItem.className = 'd-flex';
+  titleNav.textContent = 'Lists';
+  buttonAddContainer.id = 'btn-add-list';
+  titleNav.id = 'nav-title';
+  listContainer.id = 'list-container';
+  textAdd.innerText = '+ Add List';
+  textAdd.id = 'add-text';
 
-  hideContainer.className = 'hide-container';
-  itemName.className = 'list-title  ';
-  showItem.className = 'show-btn';
-  deleteItem.className = 'delete-btn';
-  showItem.value = name;
-  listItem.id = name;
-  localStorage.setItem('projectName', JSON.stringify(name));
-  if (!localStorage.getItem(name)) localStorage.setItem(name, JSON.stringify([]));
-  showItem.addEventListener('click', () => {
-    // const local = JSON.parse(localStorage.getItem('user'));
+  buttonAddContainer.appendChild(textAdd);
+  navContainer.appendChild(titleNav);
+  navContainer.appendChild(buttonAddContainer);
+  navContainer.appendChild(listContainer);
 
-    localStorage.setItem('projectName', JSON.stringify(this.value));
-    allTasks();
-  });
-
-  deleteItem.addEventListener('click', () => {
-    document.getElementById(`${name}`).remove();
-    localStorage.removeItem(name);
-    showMessage('To do List Removed.', 'rgba(255, 0, 0, 0.4)');
-    cleanTasks();
-  });
-  listItem.appendChild(itemName);
-  hideContainer.appendChild(showItem);
-  hideContainer.appendChild(deleteItem);
-
-  listItem.appendChild(hideContainer);
-  listContainer.appendChild(listItem);
+  textAdd.onclick = showMenuAdd;
+  allLists();
 }
+function mySideBar() {
+  const mainContainer = document.getElementById('row-container');
+  const navContainer = document.createElement('div');
+  navContainer.id = 'nav-container';
+  mainContainer.appendChild(navContainer);
+  renderList();
+}
+
 function showMenuAdd() {
   const addContainer = document.getElementById('btn-add-list');
   const textHide = document.getElementById('add-text');
@@ -51,6 +41,7 @@ function showMenuAdd() {
   const deleteBtn = document.createElement('button');
 
   textHide.className = 'd-none';
+
   plusBtn.id = 'btn-list-plus';
   deleteBtn.id = 'btn-list-delete';
   nameListBox.id = 'list-name-box';
@@ -91,34 +82,45 @@ function showMenuAdd() {
     textHide.className = 'd-flex  ';
   });
 }
-function renderList() {
-  const navContainer = document.getElementById('nav-container');
-  const titleNav = document.createElement('h3');
-  const buttonAddContainer = document.createElement('div');
-  const listContainer = document.createElement('div');
-  const textAdd = document.createElement('div');
 
-  titleNav.textContent = 'Lists';
-  buttonAddContainer.id = 'btn-add-list';
-  titleNav.id = 'nav-title';
-  listContainer.id = 'list-container';
-  textAdd.innerText = '+ Add List';
-  textAdd.id = 'add-text';
+function addItemToList(name) {
+  const listContainer = document.getElementById('list-container');
+  const listItem = document.createElement('div');
+  const itemName = document.createElement('div');
+  const showItem = document.createElement('div');
+  const deleteItem = document.createElement('div');
+  const hideContainer = document.createElement('div');
 
-  buttonAddContainer.appendChild(textAdd);
-  navContainer.appendChild(titleNav);
-  navContainer.appendChild(buttonAddContainer);
-  navContainer.appendChild(listContainer);
+  itemName.innerText = `${name}`;
+  deleteItem.innerText = 'X';
+  showItem.innerText = 'Show';
+  listItem.className = 'd-flex';
 
-  textAdd.onclick = showMenuAdd;
-  allLists();
-}
-function mySideBar() {
-  const mainContainer = document.getElementById('row-container');
-  const navContainer = document.createElement('div');
-  navContainer.id = 'nav-container';
-  mainContainer.appendChild(navContainer);
-  renderList();
+  hideContainer.className = 'hide-container';
+  itemName.className = 'list-title  ';
+  showItem.className = 'show-btn';
+  deleteItem.className = 'delete-btn';
+  showItem.value = name;
+  listItem.id = name;
+  localStorage.setItem('projectName', JSON.stringify(name));
+  if (!localStorage.getItem(name)) localStorage.setItem(name, JSON.stringify([]));
+  showItem.addEventListener('click', () => {
+    localStorage.setItem('projectName', JSON.stringify(showItem.value));
+    allTasks();
+  });
+
+  deleteItem.addEventListener('click', () => {
+    document.getElementById(`${name}`).remove();
+    localStorage.removeItem(name);
+    showMessage('To do List Removed.', 'rgba(255, 0, 0, 0.4)');
+    cleanTasks();
+  });
+  listItem.appendChild(itemName);
+  hideContainer.appendChild(showItem);
+  hideContainer.appendChild(deleteItem);
+
+  listItem.appendChild(hideContainer);
+  listContainer.appendChild(listItem);
 }
 
 export { mySideBar, addItemToList };
